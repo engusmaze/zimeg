5debb0e84d24147151be28d706838a9105a90917
EngusMaze <supetmiha100@gmail.com>
2024-05-25 16:11:14 +0200

    Update for Zig 0.13

--- build.zig
+++ build.zig
@@ -7,12 +7,12 @@ pub fn build(b: *std.Build) void {
     const zimeg = b.addModule("zimeg", .{
         .target = target,
         .optimize = optimize,
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
     });
 
     const exe = b.addExecutable(.{
         .name = "zimeg",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -31,7 +31,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
